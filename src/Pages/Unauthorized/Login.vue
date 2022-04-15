7c3c6220fa4554447d2aa2880c4e6a77d3fc11b6
Gustas <gustass1@gmail.com>
2022-04-15 21:15:21 +0200

    Created first iteration of Login page design

---
+++ src/Pages/Unauthorized/Login.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts"></script>
+<template>
+	<div class="flex items-center justify-center h-screen bg-amber-300">
+		<main
+			class="
+				flex flex-col
+				justify-end
+				rounded
+				w-1/4
+				h-1/4
+				bg-amber-500/60
+				text-white
+			"
+		>
+			<div class="flex flex-1 items-center justify-center text-5xl">
+				<div>DID IT</div>
+			</div>
+			<button
+				class="
+					py-2
+					px-4
+					border border-transparent
+					text-md
+					font-medium
+					rounded-b-md
+					bg-amber-500
+					hover:bg-amber-500/70
+					focus:outline-none
+					focus:ring-2
+					focus:ring-offset-2
+					focus:ring-indigo-500
+				"
+			>
+				Sign in
+			</button>
+		</main>
+	</div>
+</template>
