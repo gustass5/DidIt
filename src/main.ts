a4662325a2930dfe8ed9919e3cd141d99afb7e6f
Gustas <gustass1@gmail.com>
2022-05-15 01:25:12 +0200

    Use vue router in vue application

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './index.css';
+import router from './core/routes/router';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+app.use(router);
+
+app.mount('#app');
