2395afb3d798fb87d59c0a1a9658555a7fff029b
Gustas <gustass1@gmail.com>
2022-05-26 22:25:19 +0200

    Added NavigationMenu component

---
+++ src/components/NavigationItem.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{ name: string }>();
+</script>
+
+<template>
+	<li
+		className="p-3 m-2 rounded-r-sm text-white cursor-pointer hover:bg-gray-700 border-l-2 border-gray-800 hover:border-orange-500"
+	>
+		{{ name }}
+	</li>
+</template>
