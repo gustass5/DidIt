46a6f13b1373c6a8ceaf17842e8cc7e112078fb1
Gustas <gustass1@gmail.com>
2022-05-26 22:25:28 +0200

    Added table component

---
+++ src/components/table/Table.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TableHeader from './TableHeader.vue';
+import TableRow from './TableRow.vue';
+</script>
+
+<template>
+	<table class="min-w-full divide-y divide-gray-200">
+		<TableHeader />
+		<tbody class="bg-white divide-y divide-gray-200">
+			<TableRow />
+		</tbody>
+	</table>
+</template>
---
+++ src/components/table/TableHeader.vue
@@ -0,0 +1,83 @@
+<template>
+	<thead class="bg-gray-50">
+		<tr>
+			<th
+				scope="col"
+				class="
+					px-4
+					py-3
+					text-left text-xs
+					font-medium
+					uppercase
+					tracking-wider
+					text-gray-500
+					sm:px-6
+					whitespace-nowrap
+				"
+			>
+				<input type="checkbox" />
+			</th>
+			<th
+				scope="col"
+				class="
+					px-4
+					py-3
+					text-left text-xs
+					font-medium
+					uppercase
+					tracking-wider
+					text-gray-500
+					sm:px-6
+					whitespace-nowrap
+				"
+			>
+				Task
+			</th>
+			<th
+				scope="col"
+				class="
+					px-4
+					py-3
+					text-left text-xs
+					font-medium
+					uppercase
+					tracking-wider
+					text-gray-500
+					sm:px-6
+					whitespace-nowrap
+				"
+			></th>
+			<th
+				scope="col"
+				class="
+					px-4
+					py-3
+					text-left text-xs
+					font-medium
+					uppercase
+					tracking-wider
+					text-gray-500
+					sm:px-6
+					whitespace-nowrap
+				"
+			>
+				Members
+			</th>
+			<th
+				scope="col"
+				class="
+					px-4
+					py-3
+					text-left text-xs
+					font-medium
+					uppercase
+					tracking-wider
+					text-gray-500
+					sm:px-6
+					whitespace-nowrap
+				"
+			></th>
+			<th class="w-[0.1%]" />
+		</tr>
+	</thead>
+</template>
