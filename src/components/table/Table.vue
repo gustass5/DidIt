7b7d111a9a9b94ff73b925ffb2f35440cf67a3cb
Gustas <gustass1@gmail.com>
2022-06-08 13:31:04 +0200

    Created Table component

--- src/components/table/Table.vue
+++ src/components/table/Table.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
 import TableHeader from './TableHeader.vue';
 import TableRow from './TableRow.vue';
+
+const props = defineProps<{ user: any | null; listId: string; rows: any[] }>();
 </script>
 
 <template>
 	<table class="min-w-full divide-y divide-gray-200">
 		<TableHeader />
 		<tbody class="bg-white divide-y divide-gray-200">
-			<TableRow />
+			<TableRow
+				v-for="row in props.rows"
+				v-bind:listId="props.listId"
+				v-bind:id="row.id"
+				v-bind:data="row.data"
+				v-bind:user="user"
+			/>
 		</tbody>
 	</table>
 </template>
--- src/components/table/TableRow.vue
+++ src/components/table/TableRow.vue
@@ -1,9 +1,85 @@
+<script setup lang="ts">
+import {
+	doc,
+	getFirestore,
+	updateDoc,
+	arrayUnion,
+	arrayRemove
+} from 'firebase/firestore';
+import { ref, computed, watch } from 'vue';
+
+const props = defineProps<{
+	listId: string;
+	id: string;
+	data: any;
+	user: any | null;
+	label?: string;
+}>();
+/**
+ * Is item/task completed
+ */
+const isChecked = ref(false);
+
+const isUserResponsible = computed(() => {
+	return (
+		props.data.responsible[props.user.uid] !== undefined &&
+		props.data.responsible[props.user.uid] !== null
+	);
+});
+
+watch(isChecked, async (newIsChecked, oldIsChecked) => {
+	await handleCheckbox(newIsChecked);
+});
+
+const handleCheckbox = async (checked: boolean) => {
+	console.log({ checked });
+	if (checked) {
+		await markAsDone();
+		return;
+	}
+	await becomeResponsible();
+};
+
+/**
+ * Every items has `responsible` object
+ * If user decided to become responsible for the task we add its uid to the object with value false
+ * Each value of `responsible` objectj can be in one of three states:
+ * false - if user has not completed the task yet
+ * true - If user has completed the task
+ * null - If user no longers want to be responsible (We do not remove it from the object out of convenience)
+ */
+
+const becomeResponsible = async () => {
+	const itemRef = doc(getFirestore(), `lists/${props.listId}/items/${props.id}`);
+
+	await updateDoc(itemRef, {
+		[`responsible.${props.user.uid}`]: false
+	});
+};
+
+const markAsDone = async () => {
+	const itemRef = doc(getFirestore(), `lists/${props.listId}/items/${props.id}`);
+
+	await updateDoc(itemRef, {
+		[`responsible.${props.user.uid}`]: true
+	});
+};
+
+const cancelResponsibility = async () => {
+	const itemRef = doc(getFirestore(), `lists/${props.listId}/items/${props.id}`);
+
+	await updateDoc(itemRef, {
+		[`responsible.${props.user.uid}`]: null
+	});
+};
+</script>
+
 <template>
 	<tr key="{id}" class="h-[59px]">
 		<td class="whitespace-nowrap px-4 text-sm sm:px-6 text-gray-900">
 			<input type="checkbox" />
 		</td>
-		<td class="px-4 text-sm sm:px-6 text-gray-900">Implement dashboard</td>
+		<td class="px-4 text-sm sm:px-6 text-gray-900">{{ props.data.content }}</td>
 		<td class="px-4 text-sm sm:px-6 text-gray-900">Important</td>
 		<td class="px-4 text-sm sm:px-6 text-gray-900">
 			<div class="flex -space-x-1 overflow-hidden">
@@ -29,6 +105,12 @@
 				/>
 			</div>
 		</td>
-		<td class="px-4 text-sm sm:px-6"><input type="checkbox" /></td>
+		<td v-if="!isUserResponsible" class="px-4 text-sm sm:px-6">
+			<button v-on:click="becomeResponsible">Become responsible</button>
+		</td>
+		<td v-else class="px-4 text-sm sm:px-6">
+			<label>Mark as done: <input v-model="isChecked" type="checkbox" /></label
+			><button v-on:click="cancelResponsibility">| Leave</button>
+		</td>
 	</tr>
 </template>
