378305da50eea974421264c3152a31cffe156a74
Gustas <gustass1@gmail.com>
2022-06-09 17:03:22 +0200

    Added transitions to modals, improved modal design

--- src/components/modal/CreateForm.vue
+++ src/components/modal/CreateForm.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { Dialog, DialogPanel, DialogTitle, DialogDescription } from '@headlessui/vue';
+import {
+	Dialog,
+	DialogPanel,
+	DialogTitle,
+	DialogDescription,
+	TransitionRoot,
+	TransitionChild
+} from '@headlessui/vue';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
 import XIcon from '@heroicons/vue/solid/XIcon';
 
@@ -29,61 +36,78 @@ const onSubmit = async event => {
 </script>
 
 <template>
-	<Dialog :open="isOpen" @close="setIsOpen" class="relative z-50">
-		<div class="fixed inset-0 flex items-center justify-center">
-			<DialogPanel
-				class="
-					flex flex-col
-					w-full
-					max-w-sm
-					rounded
-					bg-white
-					p-4
-					rounded
-					shadow-sm
-				"
-			>
-				<button
-					class="flex items-center justify-end px-4"
-					@click="setIsOpen(false)"
+	<TransitionRoot appear :show="isOpen" as="template">
+		<Dialog :open="isOpen" @close="setIsOpen" class="relative z-50">
+			<div class="fixed inset-0 flex items-center justify-center">
+				<TransitionChild
+					as="template"
+					enter="duration-300 ease-out"
+					enter-from="opacity-0 scale-95"
+					enter-to="opacity-100 scale-100"
+					leave="duration-200 ease-in"
+					leave-from="opacity-100 scale-100"
+					leave-to="opacity-0 scale-95"
 				>
-					<XIcon class="h-5 w-5 text-black hover:text-gray-500" />
-				</button>
-				<span
-					class="w-full text-center text-2xl text-gray-600 mb-4 font-semibold"
-				>
-					CREATE NEW LIST
-				</span>
-				<form
-					class="flex flex-col items-center justify-center space-y-4"
-					v-on:submit="onSubmit($event)"
-				>
-					<label class="flex flex-col w-full">
-						<span class="text-gray-500 text-sm">Name</span>
-						<input
-							class="border outline-none text-gray-700"
-							type="text"
-							v-model="name"
-						/>
-					</label>
-					<button
+					<DialogPanel
 						class="
+							flex flex-col
 							w-full
-							border
-							p-2
-							rounded-md
-							bg-orange-500
-							text-white
-							font-semibold
+							max-w-sm
+							rounded
+							bg-white
+							p-4
+							rounded
+							shadow-sm
 						"
-						type="submit"
 					>
-						CREATE
-					</button>
-				</form>
-			</DialogPanel>
-		</div>
-	</Dialog>
+						<button
+							class="flex items-center justify-end px-4"
+							@click="setIsOpen(false)"
+						>
+							<XIcon class="h-5 w-5 text-black hover:text-gray-500" />
+						</button>
+						<span
+							class="
+								w-full
+								text-center text-2xl text-gray-600
+								mb-4
+								font-semibold
+							"
+						>
+							CREATE NEW LIST
+						</span>
+						<form
+							class="flex flex-col items-center justify-center space-y-4"
+							v-on:submit="onSubmit($event)"
+						>
+							<label class="flex flex-col w-full">
+								<span class="text-gray-500 text-sm">Name</span>
+								<input
+									class="border outline-none text-gray-700"
+									type="text"
+									v-model="name"
+								/>
+							</label>
+							<button
+								class="
+									w-full
+									border
+									p-2
+									rounded-md
+									bg-orange-500
+									text-white
+									font-semibold
+								"
+								type="submit"
+							>
+								CREATE
+							</button>
+						</form>
+					</DialogPanel>
+				</TransitionChild>
+			</div>
+		</Dialog>
+	</TransitionRoot>
 	<button
 		class="
 			flex
@@ -92,7 +116,7 @@ const onSubmit = async event => {
 			w-full
 			bg-orange-600
 			hover:bg-orange-700
-			text-white text-2xl
+			text-white text-xl
 			cursor-pointer
 			rounded-md
 			font-medium
@@ -100,6 +124,6 @@ const onSubmit = async event => {
 		"
 		v-on:click="setIsOpen(true)"
 	>
-		<span>Create</span>
+		<span>CREATE</span>
 	</button>
 </template>
--- src/components/modal/CreateItemForm.vue
+++ src/components/modal/CreateItemForm.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { Dialog, DialogPanel, DialogTitle, DialogDescription } from '@headlessui/vue';
+import {
+	Dialog,
+	DialogPanel,
+	DialogTitle,
+	DialogDescription,
+	TransitionRoot,
+	TransitionChild
+} from '@headlessui/vue';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
 import { XIcon } from '@heroicons/vue/solid';
 
@@ -31,61 +38,78 @@ const onSubmit = async event => {
 </script>
 
 <template>
-	<Dialog :open="isOpen" @close="setIsOpen" class="relative z-50">
-		<div class="fixed inset-0 flex items-center justify-center">
-			<DialogPanel
-				class="
-					flex flex-col
-					w-full
-					max-w-sm
-					rounded
-					bg-white
-					p-4
-					rounded
-					shadow-sm
-				"
-			>
-				<button
-					class="flex items-center justify-end px-4"
-					@click="setIsOpen(false)"
+	<TransitionRoot appear :show="isOpen" as="template">
+		<Dialog :open="isOpen" @close="setIsOpen" class="relative z-50">
+			<div class="fixed inset-0 flex items-center justify-center">
+				<TransitionChild
+					as="template"
+					enter="duration-300 ease-out"
+					enter-from="opacity-0 scale-95"
+					enter-to="opacity-100 scale-100"
+					leave="duration-200 ease-in"
+					leave-from="opacity-100 scale-100"
+					leave-to="opacity-0 scale-95"
 				>
-					<XIcon class="h-5 w-5 text-black hover:text-gray-500" />
-				</button>
-				<span
-					class="w-full text-center text-2xl text-gray-600 mb-4 font-semibold"
-				>
-					CREATE NEW LIST ITEM
-				</span>
-				<form
-					class="flex flex-col items-center justify-center space-y-4"
-					v-on:submit="onSubmit($event)"
-				>
-					<label class="flex flex-col w-full">
-						<span class="text-gray-500 text-sm">Content</span>
-						<input
-							class="border outline-none text-gray-700"
-							type="text"
-							v-model="content"
-						/>
-					</label>
-					<button
+					<DialogPanel
 						class="
+							flex flex-col
 							w-full
-							border
-							p-2
-							rounded-md
-							bg-orange-500
-							text-white
-							font-semibold
+							max-w-sm
+							rounded
+							bg-white
+							p-4
+							rounded
+							shadow-sm
 						"
-						type="submit"
 					>
-						CREATE
-					</button>
-				</form>
-			</DialogPanel>
-		</div>
-	</Dialog>
+						<button
+							class="flex items-center justify-end px-4"
+							@click="setIsOpen(false)"
+						>
+							<XIcon class="h-5 w-5 text-black hover:text-gray-500" />
+						</button>
+						<span
+							class="
+								w-full
+								text-center text-2xl text-gray-600
+								mb-4
+								font-semibold
+							"
+						>
+							ADD NEW LIST ITEM
+						</span>
+						<form
+							class="flex flex-col items-center justify-center space-y-4"
+							v-on:submit="onSubmit($event)"
+						>
+							<label class="flex flex-col w-full">
+								<span class="text-gray-500 text-sm">Content</span>
+								<input
+									class="border outline-none text-gray-700"
+									type="text"
+									v-model="content"
+								/>
+							</label>
+							<button
+								class="
+									w-full
+									border
+									p-2
+									rounded-md
+									bg-orange-500
+									text-white
+									font-semibold
+								"
+								type="submit"
+							>
+								ADD
+							</button>
+						</form>
+					</DialogPanel>
+				</TransitionChild>
+			</div>
+		</Dialog>
+	</TransitionRoot>
 	<button
 		class="
 			flex
@@ -94,7 +118,6 @@ const onSubmit = async event => {
 			hover:bg-orange-700
 			px-3
 			py-2
-			m-2
 			rounded-md
 			text-sm
 			font-medium
@@ -102,6 +125,6 @@ const onSubmit = async event => {
 		"
 		v-on:click="setIsOpen(true)"
 	>
-		Add Item
+		ADD ITEM
 	</button>
 </template>
