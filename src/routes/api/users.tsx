87ea10671acdbccb986d49bc30533a0bce614f83
gustass5 <gustass1@gmail.com>
2023-02-09 00:22:57 +0200

    Created api route to get all users

---
+++ src/routes/api/users.tsx
@@ -0,0 +1,21 @@
+import { ActionArgs } from '@remix-run/node';
+import { FirebaseServer } from '~/firebase/server/firebase.server';
+import { UserSchema } from '~/schema/Schema';
+import { Session } from '~/sessions';
+
+/**
+ * API route to get all the users in the application. It gets all the users regardless of the list
+ */
+export const action = async ({ request }: ActionArgs) => {
+	const user = await Session.isUserSessionValid(request);
+
+	if (!user) {
+		throw new Error('Not authorized');
+	}
+
+	const usersSnapshot = await FirebaseServer.database.collection('users').get();
+
+	const usersDocuments = usersSnapshot.docs.map(doc => UserSchema.parse(doc.data()));
+
+	return { users: usersDocuments };
+};
