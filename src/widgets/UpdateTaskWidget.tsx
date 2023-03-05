63f2c4d52f460506e8bba4b6368d91584f7f12d2
gustass5 <gustass1@gmail.com>
2023-03-05 18:47:41 +0200

    Implemented UpdateTaskWidget

---
+++ src/widgets/UpdateTaskWidget.tsx
@@ -0,0 +1,49 @@
+import { Dialog } from '@headlessui/react';
+import { useFetcher } from '@remix-run/react';
+import { useState } from 'react';
+import { TaskType } from '~/schema/Schema';
+
+export const UpdateTaskWidget: React.FC<{ task: TaskType }> = ({ task }) => {
+	const updateFetcher = useFetcher();
+
+	const [isOpen, setIsOpen] = useState(false);
+
+	return (
+		<>
+			<button onClick={() => setIsOpen(true)}>UPDATE</button>
+
+			<Dialog
+				open={isOpen}
+				onClose={() => setIsOpen(false)}
+				className="relative z-50"
+			>
+				<div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+				<div className="fixed inset-0 flex items-center justify-center p-4">
+					<Dialog.Panel className="w-full max-w-sm rounded bg-white">
+						<Dialog.Title>Update task</Dialog.Title>
+						<Dialog.Description>This will update task</Dialog.Description>
+
+						<updateFetcher.Form method="post">
+							<input name="taskId" type="hidden" value={task.id} />
+							<input
+								name="name"
+								type="text"
+								placeholder="Name"
+								defaultValue={task.name}
+							/>
+							<input
+								name="labels"
+								type="text"
+								placeholder="Label(s)"
+								defaultValue={task.labels}
+							/>
+							<button name="action" type="submit" value="update">
+								Update
+							</button>
+						</updateFetcher.Form>
+					</Dialog.Panel>
+				</div>
+			</Dialog>
+		</>
+	);
+};
