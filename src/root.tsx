7cf334870f9a944221b5b8e0303553c3758ee91b
gustass5 <gustass1@gmail.com>
2023-10-08 17:18:37 +0200

    Updated page title

--- src/root.tsx
+++ src/root.tsx
@@ -16,7 +16,7 @@ export function links() {
 }
 export const meta: MetaFunction = () => ({
 	charset: 'utf-8',
-	title: 'New Remix App',
+	title: 'DIDIT',
 	viewport: 'width=device-width,initial-scale=1'
 });
 export default function App() {
